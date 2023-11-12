580ce526639585aceba74d773a19d5f77076c438
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-11-12 11:58:26 -0300

    mock TCP connection

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from network.ApplicationLayer import Client
+from network.ApplicationLayer import Client, Server
 
 def main():
     client = Client()
-    client.init()
+    server = Server()
+    blocked_client = Client()
 
+    # setting the environment
+    client.setMessage() # set a message to be sent
+
+    # connection
+    client.connect(server) # works and connects both
+    blocked_client.connect(server) # fails and does not connect
 
 main()
--- network/ApplicationLayer.py
+++ network/ApplicationLayer.py
@@ -1,6 +1,44 @@
 # mock client for proof of concept
 class Client:
+
+    # constructor
+    def __init__(self):
+        self.message = ''
+        self.server = 0
     
-    def init(self):
+    # set a message to be sent
+    def setMessage(self):
         print('>> Client initialized')
-        input('>> Type your message here: ')
+        self.message = input('>> Type your message here: ')
+        print('>>')
+
+    # Attempt to connect to server
+    def connect(self, server):
+        if self.server != 0:
+            print('>> Client already connected to this or another server')
+            print('>> Connection could not be completed\n>>')
+            return False
+        if server.connect(self):
+            self.server = server
+            print('>> Client connected to server\n>>')
+            return True
+        print('>> Server is busy')
+        print('>> Connection could not be completed\n>>')
+        return False
+
+# mock server for proof of concept
+class Server:
+
+    # constructor
+    def __init__(self):
+        self.message = 'NO_MESSAGE_RECEIVED'
+        self.client = 0
+
+    # accept or deny client request to connect
+    def connect(self, client):
+        if self.client == 0:
+            self.free = False
+            self.client = client
+            return True
+        return False
+            
