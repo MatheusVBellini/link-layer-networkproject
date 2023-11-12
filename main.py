dc4d07c6404a6a9c80e64b973a5c7fb086b5b218
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-11-12 11:23:55 -0300

    project structure

---
+++ main.py
@@ -0,0 +1,8 @@
+from network.ApplicationLayer import Client
+
+def main():
+    client = Client()
+    client.init()
+
+
+main()
---
+++ network/ApplicationLayer.py
@@ -0,0 +1,6 @@
+# mock client for proof of concept
+class Client:
+    
+    def init(self):
+        print('>> Client initialized')
+        input('>> Type your message here: ')
