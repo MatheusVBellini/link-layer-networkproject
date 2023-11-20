de6a72294a9f30c127ddb54cb4f67bbb940d2814
lucasgpulcinelli <lucasegp@usp.br>
2023-11-20 17:30:36 -0300

    add error generation in physical layer

--- network/PhysicalLayer.py
+++ network/PhysicalLayer.py
@@ -1,4 +1,5 @@
 from . import time, cstr
+import random as r
 
 
 # moves the data physically, generating statistical noise and errors in the data
@@ -6,6 +7,23 @@ def transport_packet(packet, source, destination):
     print(f'>>\n>> {cstr("PHYSICAL LAYER", "cyan")}')
     print(f'>> {time()} Moving packet from one point to another...')
 
-    # IMPLEMENT: stochastic error generation
+    packet_out = bytes()
+    for i, byte_in in enumerate(packet):
+        byte_out = transport_byte(i, byte_in)
+        packet_out += byte_out
 
-    return packet, source, destination
+    return packet_out, source, destination
+
+def transport_byte(pos, value_in):
+    value_out = 0
+
+    # for each bit
+    for i in range(8):
+        # try to transport it, or fail
+        if r.randint(0, 99) != 0:
+            value_out |= value_in & (1 << i)
+        else:
+            value_out |= (value_in ^ 0b11111111) & (1 << i)
+            print(f'>> {time()} flipped bit {i} of byte {pos}')
+
+    return int.to_bytes(value_out)
