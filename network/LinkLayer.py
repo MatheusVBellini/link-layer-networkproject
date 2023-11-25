1d71b6ca54eae4b4e47ba84afee9af2ac721923c
lucasgpulcinelli <lucasegp@usp.br>
2023-11-25 18:41:22 -0300

    let the user choose the parity type it wants

--- network/LinkLayer.py
+++ network/LinkLayer.py
@@ -6,16 +6,39 @@ import network.PhysicalLayer as pl
 # framed packet structure
 framedPacket = namedtuple('framedPacket', ['content'])
 
+even_parity = True
+
+
+def setup_parity():
+    global even_parity
+
+    while True:
+        parity_type = input('>> type which parity error checking you want: ')
+
+        if parity_type == 'even':
+            even_parity = True
+            return
+        if parity_type == 'odd':
+            even_parity = False
+            return
+        print(
+            f'>> {time()} invalid parity type selected, only "even" or "odd" accepted')
 
 # frame the packet and send it through the physical connection
+
+
 def frame_packet(packet, source, destination):
     print(f'>>\n>> {cstr("LINK LAYER - CLIENT", "cyan")}')
+
+    setup_parity()
+
     print(f'>> {time()} Framing packet')
 
     bin_packet = packet.encode('utf-8')  # get packet bits
     uint_crc = crc_calc(bin_packet)  # get crc of the packet
-    bin_crc = uint_crc.to_bytes(length=4, byteorder='big', signed=False)  # byte representation of the crc
-    bin_parity = odd_parity(bin_packet)  # returns a odd parity check byte
+    # byte representation of the crc
+    bin_crc = uint_crc.to_bytes(length=4, byteorder='big', signed=False)
+    bin_parity = parity(bin_packet, even_parity)  # returns a parity check byte
 
     algorithm = 'flagbytes'  # define the framing algorithm to be Flag Bytes
 
@@ -26,7 +49,8 @@ def frame_packet(packet, source, destination):
 
     framed_packet = frame(bin_crc + bin_parity + bin_packet)
 
-    print(f'>> {time()}   Framed packet: {cstr(bytes_to_bits(framed_packet), "green")}')
+    print(
+        f'>> {time()}   Framed packet: {cstr(bytes_to_bits(framed_packet), "green")}')
 
     pl.transport_packet(framed_packet, source, destination)
 
@@ -43,9 +67,11 @@ def unpack_packet(packet, source, destination):
     print(f'>> {time()} Detecting errors:')
 
     received_crc = packet[1:5]  # what CRC currently is
-    received_parity = packet[5:6]  # what the odd parity bit is
-    calculated_crc = crc_calc(unframed_packet).to_bytes(length=4, byteorder='big', signed=False)  # what CRC should be
-    calculated_parity = odd_parity(unframed_packet)  # what odd parity bit should be
+    received_parity = packet[5:6]  # what the parity bit is
+    calculated_crc = crc_calc(unframed_packet).to_bytes(
+        length=4, byteorder='big', signed=False)  # what CRC should be
+    # what parity bit should be
+    calculated_parity = parity(unframed_packet, even_parity)
 
     if received_crc == calculated_crc:
         print(f'>> {time()} CRC {cstr("OK", "green")}')
@@ -53,9 +79,11 @@ def unpack_packet(packet, source, destination):
         print(f'>> {time()} CRC {cstr("FAILED", "red")}')
 
     if received_parity == calculated_parity:
-        print(f'>> {time()} Odd Parity Bit {cstr("OK", "green")}')
+        print(
+            f'>> {time()} {"Even" if even_parity else "Odd"} Parity Bit {cstr("OK", "green")}')
     else:
-        print(f'>> {time()} Odd Parity Bit {cstr("FAILED", "red")}')
+        print(
+            f'>> {time()} {"Even" if even_parity else "Odd"} Parity Bit {cstr("FAILED", "red")}')
 
     destination.receive(unframed_packet)
 
@@ -70,9 +98,12 @@ def crc_calc(packet):
     return crc32(packet)
 
 
-# calculate the odd parity bit of a byte stream
-def odd_parity(packet):
-    return ((1 + len(list(filter(lambda x: x == '1', bytes_to_bits(packet))))) % 2).to_bytes(length=1, byteorder='big',
+# calculate the parity bit of a byte stream
+def parity(packet, is_even=False):
+    v = 1
+    if is_even:
+        v = 0
+    return ((v + len(list(filter(lambda x: x == '1', bytes_to_bits(packet))))) % 2).to_bytes(length=1, byteorder='big',
                                                                                              signed=False)
 
 
